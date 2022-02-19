bb3929d5138c11838448f62c359f88804b663085
arslan456 <muhammadarslanjabbar@gmail.com>
2022-02-19 22:06:53 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import { AppBar, Box, Toolbar, Typography, InputBase } from '@material-ui/core';
+import { Autocomplete } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import useStyles from './style.js';
+import SearchIcon from '@material-ui/icons/Search';
+
+const Header = ({ setCoordinates }) => {
+  const classes = useStyles();
+  const [autocomplete, setAutocomplete] = useState(null);
+
+  const onLoad = (autoC) => setAutocomplete(autoC);
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+
+    setCoordinates({ lat, lng })
+  }
+
+  return (
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display='flex'>
+          <Typography variant='h5' className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+};
+
+export default Header;
